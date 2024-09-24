73fd27ac23b8c5e621544fe68f0bb16d8f17b58c
Shaon Ahmed <shaon.ahmed@hackberry.se>
2024-09-24 15:47:09 +0200

    Add /results endpoint to server & update git ignore

--- server/server.js
+++ server/server.js
@@ -1,8 +1,47 @@
 const express = require("express");
 const client = require("./elasticsearch/client");
+const cors = require("cors");
+
 const app = express();
 const port = 3001;
 
+app.use(cors());
+
+app.get("/results", (req, res) => {
+  const amountOfEmployees = req.query["Antal anställda"];
+  const position = req.query["Befattning"];
+  const county = req.query["ort"];
+  const turnover = req.query["omsättning"];
+  const sortOption = req.query["sortOption"];
+
+  async function sendESRequest() {
+    const body = await client.search({
+      index: "test",
+      body: {
+        sort: [{}],
+        size: 100,
+        query: {
+          bool: {
+            filter: [
+              // {
+              //   term: { "Antal anställda": amountOfEmployees },
+              // },
+              // {
+              //   range: { omsättning: { gte: turnover } },
+              // },
+              {
+                match: { Ort: county },
+              },
+            ],
+          },
+        },
+      },
+    });
+    console.log(body);
+    res.json(body.hits.hits);
+  }
+  sendESRequest();
+});
 app.listen(port, () =>
   console.log(`Server listening at http://localhost:${port}`)
 );
